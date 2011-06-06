b78fa39533661b3055bcc42e2d87ff6162474dc1
Robert Kaye <rob@partyrobotics.com>
2011-06-05 19:04:49 -0700

    Fixed links and changed desc to a textarea

--- ui/bartendro/form/booze.py
+++ ui/bartendro/form/booze.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-from wtforms import Form, TextField, DecimalField, HiddenField, validators
+from wtforms import Form, TextField, DecimalField, HiddenField, validators, TextAreaField
 
 class BoozeForm(Form):
     id = HiddenField(u"id", default=0)
     name = TextField(u"Name", [validators.Length(min=3, max=255)])
     brand = TextField(u"Brand", [validators.Length(min=3, max=255)])
-    desc = TextField(u"Description", [validators.Length(min=3, max=1024)])
+    desc = TextAreaField(u"Description", [validators.Length(min=3, max=1024)])
     abv = DecimalField(u"ABV", [validators.NumberRange(0, 97)], default=0, places=0)
 
 form = BoozeForm()
