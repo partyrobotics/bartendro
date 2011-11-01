798018f1ad7342f5494782e162ee7fa5b2cd8eca
Robert Kaye <rob@musicbrainz.org>
2011-10-31 22:00:54 -0700

    Added the beginnings of the make drink page.

--- ui/bartendro/views.py
+++ ui/bartendro/views.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from bartendro.view import root
-from bartendro.view.admin import admin, booze, drink
+from bartendro.view.admin import admin, booze as booze_admin, drink as drink_admin
+from bartendro.view.drink import drink
 from bartendro.view.ws import booze as ws_booze
 
 view_map = {}
 view_map['bartendro.view.root'] =  root
 view_map['bartendro.view.ws.booze'] =  ws_booze
 view_map['bartendro.view.admin.admin'] =  admin
-view_map['bartendro.view.admin.booze'] = booze
-view_map['bartendro.view.admin.drink'] = drink
+view_map['bartendro.view.admin.booze'] = booze_admin
+view_map['bartendro.view.admin.drink'] = drink_admin
+view_map['bartendro.view.drink.drink'] = drink
