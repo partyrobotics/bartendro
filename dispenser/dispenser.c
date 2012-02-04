aafe08b20d2365980e92864c5224e66dc04579a5
Robert Kaye <rob@partyrobotics.com>
2012-02-04 10:21:54 -0800

    Updated dispenser code for serial daisy chain

--- dispenser/dispenser.c
+++ dispenser/dispenser.c
@@ -34,22 +34,98 @@
 #define TIMER1_INIT 0xFF06 // 8mhz / 8 cs / 1000 = 1ms per 'tick'
 #endif
 
-#define DEBUG 0
+static uint8_t g_address = 0xFF;
+static uint8_t g_response_payload[2] = { 0, 0};
+static uint8_t g_motor_state = 0;
 
-volatile uint8_t g_motor_state = 0;
+static volatile uint8_t g_ss_reset = 0;
+static volatile uint8_t g_hall_sensor_1 = 0;
+static volatile uint8_t g_hall_sensor_2 = 0;
+
+uint8_t set_motor_state(uint8_t state);
+
+#define DEBUG 1
+
+ISR(PCINT0_vect)
+{
+    if (PINB & (1<<PINB2))
+        g_ss_reset = 1;
+    else
+        g_ss_reset = 0;
+}
 
 ISR(PCINT1_vect)
 {
-    if (PINC & (1<<PINC3))
+    if (PINC & (1<<PINC0))
+        g_hall_sensor_1++;
+    if (PINC & (1<<PINC1))
+        g_hall_sensor_2++;
+}
+
+// clock globals
+volatile uint32_t ticks = 0;
+volatile uint8_t  motor_state = 0;
+volatile uint16_t dispense_ticks = 0;
+volatile uint8_t  dispense_chunks = 0;
+
+#define DISPENSE_TICKS  700
+#define DISPENSE_DELAY 1000
+
+ISR (TIMER1_OVF_vect)
+{
+    if (ticks == 0)
     {
-        cbi(PORTB, 1);
-        g_motor_state = 0;
+        if (motor_state)
+            set_motor_state(0);
+
+        if (dispense_chunks == 0)
+        {
+            TIMSK1 &= ~(1<<TOIE1);
+            return;
+        }
+
+        if (motor_state)
+        {
+            ticks = DISPENSE_DELAY;
+            motor_state = 0;
+        }
+        else
+        {
+            ticks = dispense_ticks;
+            dispense_chunks--;
+            
+            set_motor_state(1);
+            motor_state = 1;
+        }
+        TCNT1 = TIMER1_INIT;
+        return;
     }
-    else 
-    {
-        sbi(PORTB, 1);
-        g_motor_state = 1;
-    }
+
+    ticks--;
+    TCNT1 = TIMER1_INIT;
+}
+
+void set_timer(uint16_t dur)
+{
+    dispense_chunks = (dur + DISPENSE_TICKS - 1) / DISPENSE_TICKS;
+    dispense_ticks = (dur + dispense_chunks - 1) / dispense_chunks;
+
+    dispense_chunks--;
+
+    ticks = dispense_ticks;
+    TCNT1 = TIMER1_INIT;
+    set_motor_state(1);
+    motor_state = 1;
+    TIMSK1 |= (1<<TOIE1);
+}
+
+void stop_timer(void)
+{
+    cli();
+    TIMSK1 &= ~(1<<TOIE1);
+    ticks = 0;
+    dispense_chunks = 0;
+    sei();
 }
 
 void serial_init(void)
@@ -67,20 +143,55 @@ void serial_tx(unsigned char ch)
     while ( !( UCSR0A & (1<<UDRE0)) );
     UDR0 = ch;
 }
-#define MAX 80 
-void dprintf(const char *fmt, ...)
+unsigned char serial_rx(void)
 {
-    va_list va;
-    va_start (va, fmt);
-    char buffer[MAX];
-    char *ptr = buffer;
-    vsnprintf(buffer, MAX, fmt, va);
-    va_end (va);
-    for(ptr = buffer; *ptr; ptr++)
+    while ( !(UCSR0A & (1<<RXC0))) 
+        ;
+
+    return UDR0;
+}
+uint8_t serial_transfer(uint8_t tx)
+{
+    uint8_t in;
+
+    in = serial_rx();
+    serial_tx(tx);
+
+    return in;
+}
+
+uint8_t receive_packet(packet *rx)
+{
+    static uint8_t ch = 0;
+    uint8_t *prx = (uint8_t*)rx;
+    uint8_t received = 0, old, reset = 0;
+
+    for(; received < sizeof(packet);)
     {
-        if (*ptr == '\n') serial_tx('\r');
-        serial_tx(*ptr);
+        cli();
+        reset = g_ss_reset;
+        sei();
+        if (reset)
+            return 0;
+
+        old = ch;
+        ch = serial_transfer(ch);
+
+        // Look for the packet header
+        if (prx == (uint8_t*)rx && ch != 0xFF)
+            continue;
+
+
+        *prx = ch;
+        prx++;
+        received++;
+
+        if (received == 5 && rx->type == PACKET_TYPE_RESPONSE)
+            ch = g_response_payload[0];
+        if (received == 6 && rx->type == PACKET_TYPE_RESPONSE)
+            ch = g_response_payload[1];
     }
+    return 1;
 }
 
 void setup(void)
@@ -91,9 +202,6 @@ void setup(void)
     // Set Motor pin as output
     DDRB |= (1<<PB1) | (1<<PB0);
 
-    // Enable the pull up on the input pin
-    sbi(PORTC, 3);
-
     // Motor driver pins
     // pin 4 high = /STANDBY
     // pin 7 high = IN1
@@ -102,9 +210,16 @@ void setup(void)
     sbi(PORTD, 7);
     cbi(PORTB, 0);
 
-    // External interrupts for motor line
-    PCMSK1 |= (1<<PCINT11);
+    // External interrupts for the reset line
+    PCMSK0 |= (1<<PCINT2);
+    PCICR |= (1<<PCIE0);
+
+    // External interrupts for the hall sensors on the motor`
+    PCMSK1 |= (1<<PCINT8)|(1<<PCINT9);
     PCICR |= (1<<PCIE1);
+
+    // Timer setup for dispense timing
+    TCCR1B |= _BV(CS11)|(1<<CS10); // clock / 64 / 256 = 244Hz = .001024 per tick
 
 #if DEBUG
     serial_init();
@@ -148,43 +263,172 @@ void set_led_red(uint8_t v)
 
 void set_led_green(uint8_t v)
 {
-    OCR0A = 255 - v;
+    OCR0B = 255 - v;
 }
 
 void set_led_blue(uint8_t v)
 {
-    OCR0B = 255 - v;
+    OCR0A = 255 - v;
 }
 
-int main(void)
+uint8_t set_motor_state(uint8_t state)
 {
-    uint8_t state;
+    uint8_t cur;
 
-	setup();
-
-    // turn the motor off, just in case
-    cbi(PORTB, 1);
-
-    led_pwm_setup();
-    set_led_color(0, 0, 0);
-
-#if DEBUG
-    dprintf("slave starting\n");
-#endif
+    cli();
+    cur = g_motor_state;
     sei();
+
+    if (cur == state)
+        return 0;
+
+    if (state)
+    {
+        cli();
+        g_motor_state = 1;
+        sei();
+        sbi(PORTB, 1);
+    }
+    else
+    {
+        cli();
+        g_motor_state = 0;
+        sei();
+        cbi(PORTB, 1);
+    }
+    return 1;
+}
+
+void wait_for_reset()
+{
+    uint8_t count = 0, reset, t = 1;
 
     for(;;)
     {
         cli();
-        state = g_motor_state;
+        reset = g_ss_reset;
         sei();
-#if DEBUG
-        dprintf("%d\n", state);
-#endif
-        set_led_red(255);
-        _delay_ms(250);
-        set_led_red(0);
-        _delay_ms(250);
+        if (!reset)
+        {
+            _delay_ms(1);
+            count++;
+        }
+        else
+            break;
+        if (count == 100)
+        {
+           set_led_color(t * 255, 0, 0);
+           t = !t;
+           count = 0;
+        }
+    }
+    set_led_color(0, 0, 0);
+}
+
+void address_assignment(void)
+{
+    uint8_t ch = 0;
+
+    for(;;)
+    {
+        ch = serial_transfer(ch);
+        if (ch > 0 && ch < 0xff)
+        {
+            g_address = ch;
+            ch++;
+            serial_transfer(ch);
+            break;
+        }
     }
 }
 
+int main(void)
+{
+    packet  p;
+    uint8_t i;
+
+	setup();
+    // turn the motor off, just in case
+    cbi(PORTB, 1);
+
+    led_pwm_setup();
+
+    sei();
+
+    wait_for_reset();
+    address_assignment();
+
+    set_led_color(0, 0, 0);
+
+    for(;;)
+    {
+        if (!receive_packet(&p))
+        {
+            // If SS went high, reset and start over
+            stop_timer();
+            set_motor_state(0);
+            g_address = 0;
+            wait_for_reset();
+            address_assignment();
+            continue;
+        }
+
+        // If we have no address yet, ignore all packets
+        if (g_address == 0)
+        {
+            continue;
+        }
+
+        if (p.addr != g_address)
+            continue;
+        if (p.type == PACKET_TYPE_START)
+        {
+            uint8_t r;
+
+            r = set_motor_state(1);
+        }
+        else
+        if (p.type == PACKET_TYPE_CHECK)
+        {
+            g_response_payload[0] = 0;
+            g_response_payload[1] = 0;
+        }
+        else
+        if (p.type == PACKET_TYPE_GETSTATE)
+        {
+            cli();
+            g_response_payload[0] = g_motor_state;
+            sei();
+            g_response_payload[1] = 0;
+        }
+        else
+        if (p.type == PACKET_TYPE_SETLED)
+        {
+            if (p.payload.ch[0] == 0)
+                set_led_red(p.payload.ch[1]);
+            if (p.payload.ch[0] == 1)
+                set_led_green(p.payload.ch[1]);
+            if (p.payload.ch[0] == 2)
+                set_led_blue(p.payload.ch[1]);
+        }
+        else
+        if (p.type == PACKET_TYPE_RESPONSE)
+        {
+            ;
+        }
+        else
+        if (p.type == PACKET_TYPE_STOP)
+        {
+            set_motor_state(0);
+        }
+        else
+        if (p.type == PACKET_TYPE_DISPENSE)
+        {
+            uint16_t temp;
+            cli();
+            temp = p.payload.word;
+            sei();
+            set_timer(temp);
+        }
+    }
+}
