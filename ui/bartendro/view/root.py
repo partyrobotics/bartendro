94f8c05774416d1aba11d57a950db92b1587f804
Robert Kaye <rob@partyrobotics.com>
2012-02-07 00:55:58 -0800

    Synthsize the list of ingredients. Show the description in the index

--- ui/bartendro/view/root.py
+++ ui/bartendro/view/root.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from operator import attrgetter
 from bartendro.utils import session, render_template, render_json, expose, validate_url, url_for
 from bartendro.model.drink import Drink
 from bartendro.model.drink_name import DrinkName
@@ -7,4 +8,12 @@ from bartendro.model.drink_name import DrinkName
 def index(request):
     drinks = session.query(Drink).join(DrinkName).filter(Drink.name_id == DrinkName.id) \
                                  .order_by(DrinkName.name).all()
+    for drink in drinks:
+        ing = []
+
+        drink.drink_boozes = sorted(drink.drink_boozes, key=attrgetter('booze.abv', 'booze.name'), reverse=True)
+        for db in drink.drink_boozes:
+            ing.append(db.booze.name)
+        drink.ingredients = ', '.join(ing)
+            
     return render_template("index", drinks=drinks)
