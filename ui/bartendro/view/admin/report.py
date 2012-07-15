6a3b954a84c89a0996272304431d0debf56cca96
Robert Kaye <rob@musicbrainz.org>
2012-07-14 23:30:37 -0700

    Added support for a simple top drinks report

--- ui/bartendro/view/admin/report.py
+++ ui/bartendro/view/admin/report.py
@@ -9,10 +9,37 @@ from bartendro.form.booze import BoozeForm
 
 @expose('/admin/report/<begin>/<end>')
 def view_report(request, begin, end):
-    begindate = int(time.mktime(time.strptime(begin, "%Y-%m-%d")))
-    enddate = int(time.mktime(time.strptime(end, "%Y-%m-%d")))
-    top_drinks = session.query("name", "cnt", "total").from_statement("""select drink_name.name, count(drink_log.drink_id) as cnt, 
-                      sum(drink_log.size) as total from drink_log, drink_name where drink_log.drink_id = drink_name.id and 
-                      drink_log.time >= :begin and drink_log.time <= :end group by drink_name.name order 
-                      by count(drink_log.drink_id) desc;""").params(begin=begindate, end=enddate).all()
-    return render_template("admin/report", top_drinks = top_drinks, title="Top Drinks", begin=begin, end=end)
+    begindate = int(time.mktime(time.strptime(begin + " 0:00", "%Y-%m-%d %H:%M")))
+    enddate = int(time.mktime(time.strptime(end + " 23:59", "%Y-%m-%d %H:%M")))
+
+    total_number = session.query("number")\
+                 .from_statement("""SELECT count(*) as number
+                                      FROM drink_log 
+                                     WHERE drink_log.time >= :begin 
+                                       AND drink_log.time <= :end""")\
+                 .params(begin=begindate, end=enddate).first()
+
+    total_volume = session.query("volume")\
+                 .from_statement("""SELECT sum(drink_log.size) as volume 
+                                      FROM drink_log 
+                                     WHERE drink_log.time >= :begin 
+                                       AND drink_log.time <= :end""")\
+                 .params(begin=begindate, end=enddate).first()
+
+    top_drinks = session.query("name", "number", "volume")\
+                 .from_statement("""SELECT drink_name.name,
+                                           count(drink_log.drink_id) AS number, 
+                                           sum(drink_log.size) AS volume 
+                                      FROM drink_log, drink_name 
+                                     WHERE drink_log.drink_id = drink_name.id 
+                                       AND drink_log.time >= :begin AND drink_log.time <= :end 
+                                  GROUP BY drink_name.name 
+                                  ORDER BY count(drink_log.drink_id) desc;""")\
+                 .params(begin=begindate, end=enddate).all()
+
+    return render_template("admin/report", top_drinks = top_drinks, 
+                                           title="Top Drinks", 
+                                           total_number=total_number[0],
+                                           total_volume=total_volume[0],
+                                           begin=begin, 
+                                           end=end)
