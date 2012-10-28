6bc313b5997b25ce1632603b95771c8858c67656
Robert Kaye <rob@musicbrainz.org>
2012-10-28 14:38:34 -0700

    Added the UI portions of the liquid out sensor test page

---
+++ ui/bartendro/view/admin/liquidout.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from werkzeug.utils import redirect
+from bartendro.utils import session, render_template, render_json, expose, validate_url, url_for, local
+from bartendro.model.drink import Drink
+from bartendro.model.booze import Booze
+from bartendro.model.booze_group import BoozeGroup
+from bartendro.form.liquidout import LiquidOutTestForm
+
+@expose('/admin/liquidout')
+def view(request):
+    form = LiquidOutTestForm(request.form)
+    return render_template("admin/liquidout", form=form, title="Liquid out test")
+
+@expose('/admin/liquidout/test')
+def save(request):
+
+    form = LiquidOutTestForm(request.form)
+    #if request.method == 'POST' and form.validate():
+    dispenser = int(request.form.get("dispenser") or '1') - 1
+    threshold = int(request.form.get("threshold") or '0')
+    print "Dispenser %d threshold %d" % (dispenser, threshold)
+
+    return render_template("admin/liquidout", form=form, title="Liquid out test")
