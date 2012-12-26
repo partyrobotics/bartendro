f7b905817ae2dc70580172651247646858af969a
Robert Kaye <rob@musicbrainz.org>
2012-12-26 15:55:08 -0800

    UI portions of the shotbot

--- ui/bartendro/master/driver.py
+++ ui/bartendro/master/driver.py
@@ -65,196 +65,49 @@ class MasterDriver(object):
         self.log("Opened %s for %d baud N81" % (self.device, BAUD_RATE))
         log("Opened %s for %d baud N81" % (self.device, BAUD_RATE))
 
-    def chain_init(self):
-        if self.software_only: 
-            self.num_dispensers = 15
-            return
-
-        log("initialize communication chain")
-        self.log("initialize communication chain")
-
-        # reset the chain
-        print "send reset"
-        self.ss.low()
-        sleep(1)
-
-        self.ss.high()
-        sleep(.1)
-        self.ss.low()
-        sleep(.1)
-
-        print "address assignment"
-        while True:
-            self.ser.write("0\n")
-            r = self.ser.readline()
-            if len(r) == 0:
-                continue
-            break
-
-        if len(r) > 0:
-            r= r[0:-1]
-	    print "received '%s'" % r
-            num = int(r)
-            if num < 1 or num > MAX_DISPENSERS:
-		msg = "Found an invalid number of dispensers. Communication chain busted!"
-                self.log(msg)
-		error(msg)
-                self.num_dispensers = -1;
-            else: 
-		msg = "found %d dispensers" % int(r)
-                self.log(msg)
-		error(msg)
-		self.num_dispensers = int(r)
-                sleep(1)
-        else:
-            error("Cannot communicate with dispenser chain!")
-            self.log("Cannot communicate with dispenser chain!")
-
     def close(self):
         if self.software_only: return
         self.ser.close()
         self.ser = None
 
-    def send(self, cmd):
-        if self.software_only: return
-        self.ser.write(cmd)
-        self.log("w: '%s'\n" % cmd.replace("\n", ""))
-        ret = self.ser.readline()
-        if ret == "": 
-            msg = "Serial comms timeout after cmd '%s'." % cmd[0:len(cmd)-1]
-            error(msg)
-            self.log(msg)
-        else:
-            self.log("r: '%s'\n" % ret.replace("\n", ""))
-        return ret
+    def chain_init(self):
+        return True
+
+    def make_shot(self):
+        return True
 
     def count(self):
-        return self.num_dispensers
+        return 1
 
     def start(self, dispenser):
-        return self.send("%d on\n" % dispenser)
+        return True
 
     def stop(self, dispenser):
-        return self.send("%d off\n" % dispenser)
+        return True
 
     def dispense_time(self, dispenser, duration):
-        return self.send("%d timedisp %d\n" % (dispenser, duration))
+        return True
 
     def dispense_ticks(self, dispenser, ticks):
-        return self.send("%d tickdisp %d\n" % (dispenser, ticks))
+        return True
 
     def led(self, dispenser, r, g, b):
-        return self.send("%d led %d %d %d\n" % (dispenser, r, g, b))
+        return True
 
     def is_dispensing(self, dispenser):
-        '''expects "!3 isdisp 1" '''
-
-        if self.software_only: return False
-
-        self.send("%d isdisp\n" % dispenser)
-        ret = self.ser.readline()
-        if not ret: 
-            msg = "is_dispensing timeout!"
-            self.log(msg)
-            error(msg)
-	    return False
-        try:
-            self.log("r: '%s'\n" % ret.replace("\n", ""))
-            disp, cmd, value = ret.split(" ")
-	    if value[0] == '1': 
-	        return True
-	    else:
-	        return False
-        except ValueError:
-            self.log("parse error: '%s'" % ret)
-	    return False
+        return False
 
     def get_liquid_level(self, dispenser):
-        '''expects "!3 level 69" '''
-
-        if self.software_only: return int(random.random() * 300)
-
-        while True:
-            self.send("%d level\n" % dispenser)
-            ret = self.ser.readline()
-            if not ret: 
-                msg = "got level timeout!"
-                self.log(msg)
-                error(msg)
-                continue
-            try:
-                self.log("r: '%s'\n" % ret.replace("\n", ""))
-                disp, cmd, value = ret.split(" ")
-                return int(value)
-            except ValueError:
-                self.log("parse error: '%s'" % ret)
+        return 50
 
     def ping(self, dispenser):
-        '''expects "!3 pong" '''
-        if self.software_only: return True
-
-        self.send("%d ping\n" % dispenser)
-        ret = self.ser.readline()
-        if not ret: 
-            msg = "ping response timeout"
-            self.log(msg)
-            error(msg)
-	    return False
-
- 	ret = ret[:-1]
-        try:
-            ret = ret[1:] # strip off the !
-            disp, cmd = ret.split(" ")
- 	    disp = int(disp)
-	    if disp == dispenser: 
-	        return True
-	    else:
-                msg = "wrong dispenser number in pong"
-                self.log("wrong dispenser number in pong")
-                error(msg)
-	        return False
-        except ValueError:
-            msg = "error parsing pong data. response: '%s'" % ret
-            self.log(msg)
-            error(msg)
-	    return False
+        return True
 
     def get_dispense_stats(self, dispenser):
-        '''expects "!3 dispstats <time> <ticks>" '''
-        if self.software_only: return True
-
-        self.send("%d dispstat\n" % dispenser)
-        ret = self.ser.readline()
-        print "'%s'\n" % ret
-        if not ret: 
-            msg = "dispstat response timeout"
-            self.log(msg)
-            error(msg)
-            return (-1, -1)
-
- 	ret = ret[:-1]
-        try:
-            ret = ret[1:] # strip off the !
-            disp, cmd, t, ticks = ret.split(" ")
- 	    disp = int(disp)
-	    if disp == dispenser: 
-	        return (-1, -1)
-	    else:
-                msg = "wrong dispenser number in pong"
-                self.log("wrong dispenser number in pong")
-                error(msg)
-	        return False
-            t = int(t)
-            ticks = int(ticks)
-            return (t, ticks)
-        except ValueError:
-            msg = "error parsing pong data. response: '%s'" % ret
-            self.log(msg)
-            error(msg)
-	    return (-1, -1)
+        return (0, 0)
 
 if __name__ == "__main__":
-    md = MasterDriver("/dev/ttyS1");
+    md = MasterDriver("/dev/ttyAMA0");
     md.open()
     md.chain_init()
     sleep(1)
--- ui/bartendro/view/drink/drink.py
+++ ui/bartendro/view/drink/drink.py
@@ -44,8 +44,8 @@ def view(request, id):
 
     show_sweet_tart = has_sweet and has_tart
     show_strength = has_alcohol and has_non_alcohol
-    show_size = 0
-    show_taster = 0 #not show_sobriety
+    show_size = 1
+    show_taster = 1 #0 #not show_sobriety
 
     if not custom_drink:
         return render_template("drink/index", 
