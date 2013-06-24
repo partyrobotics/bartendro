56d0c7237ab9c6d30dbb52fa4a324d0481d44abb
Robert Kaye <rob@partyrobotics.com>
2013-06-24 22:42:32 +0200

    Added some styling to the log page and finished linking between pages.

--- ui/bartendro/view/blender.py
+++ ui/bartendro/view/blender.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from sqlalchemy import func, asc
 import memcache
+import json
 from bartendro import app, db
 from flask import Flask, request, redirect, render_template
 from flask.ext.login import login_required
 from bartendro.model.drink import Drink
 from bartendro.model.booze import Booze
 from bartendro.model.dispenser import Dispenser
+from bartendro.model.blender_log import BlenderLog
 
 @app.route('/blender')
 def blender():
@@ -27,4 +29,25 @@ def blender():
                            boozes=boozes,
                            count=count,
                            recipe=recipe,
+                           options=app.options)
+
+@app.route('/blender/log')
+#@login_required
+def blender_log():
+    boozes = db.session.query(Booze).order_by(Booze.id).all()
+    log = BlenderLog.query.all()
+    blends = []
+    args = []
+    for entry in log:
+        blend = []
+        data = json.loads(entry.blend)
+        for k, v in data:
+            blend.append({ 'name' : boozes[k].name, 'value' : v})
+            args.append("booze%d=%d" % (boozes[k].id, v))
+        url = "/blender?" + "&".join(args)
+        blends.append({ 'number' : entry.id, 'blend' : blend, 'url' : url })
+
+    return render_template("blender-log",
+                           title="Previous blends",
+                           blends=blends,
                            options=app.options)
