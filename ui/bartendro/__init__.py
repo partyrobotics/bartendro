a4d89f3ddd883c412dd46753d82cda4534f0e2e0
Robert Kaye <rob@partyrobotics.com>
2013-06-24 20:12:22 +0200

    Debugged and cleaned up the ws functions

--- ui/bartendro/__init__.py
+++ ui/bartendro/__init__.py
@@ -66,4 +66,4 @@ from bartendro.view.admin import booze as booze_admin, drink as drink_admin, \
                                  dispenser as admin_dispenser, report, liquidlevel, user, options, debug
 from bartendro.view.drink import drink
 from bartendro.view.ws import booze as ws_booze, dispenser as ws_dispenser, drink as ws_drink, \
-                              misc as ws_misc, liquidlevel, option as ws_options, blend_log
+                              misc as ws_misc, liquidlevel, option as ws_options, blender_log
--- ui/bartendro/view/ws/blender_log.py
+++ ui/bartendro/view/ws/blender_log.py
@@ -8,43 +8,38 @@ from flask.ext.login import login_required, current_user
 from werkzeug.exceptions import ServiceUnavailable, BadRequest, InternalServerError
 from bartendro.model.blender_log import BlenderLog
 
-@app.route('/ws/blend-log/load')
+@app.route('/ws/blender-log')
 #@login_required
-def blend_log_load():
+def blender_log_load():
     log = BlenderLog.query.all()
     blends = []
     for blend in log:
-        blends.append((blend.id, booze.blend))
+        blends.append((blend.id, blend.blend))
     return json.dumps({ 'log' : blends})
 
-@app.route('/ws/blend-log/assign')
+@app.route('/ws/blender-log/assign')
 #@login_required
-def blend_log_assign():
+def blender_log_assign():
 
     arg_blend = []
-    arg_dict = {}
     for arg in request.args:
         n = int(arg[5:])
         v = int(request.args.get(arg))
         arg_blend.append((n, v))
-        arg_dict[n] = v
     arg_set = set(arg_blend)
 
     log = BlenderLog.query.all()
     for entry in log:
         blend = []
         data = json.loads(entry.blend)
-        for key in data:
-            blend.append((int(key), data[key]))
+        for k, v in data:
+            blend.append((int(k), int(v)))
         this_set = set(blend)
-        print this_set
-        print arg_set
-        print
         if this_set == arg_set:
             return json.dumps({ 'id' : entry.id })
 
     # save new blend to disk
-    blend = BlenderLog(json.dumps(arg_dict))
+    blend = BlenderLog(json.dumps(arg_blend))
     db.session.add(blend)
     db.session.commit()
 
