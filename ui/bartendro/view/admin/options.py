49cc2ed44565064431b43cc49e405b14a30beb86
Robert Kaye <rob@musicbrainz.org>
2013-06-24 19:44:04 -0700

    Upload to dropbox partially implemented

--- ui/bartendro/view/admin/options.py
+++ ui/bartendro/view/admin/options.py
@@ -4,9 +4,22 @@ from bartendro import app, db
 from flask import Flask, request, render_template
 from flask.ext.login import login_required
 from bartendro.model.version import DatabaseVersion
+import dropbox
 
 @app.route('/admin/options')
 @login_required
 def admin_options():
     ver = DatabaseVersion.query.one()
     return render_template("admin/options", options=app.options, title="Options", schema = ver.schema)
+
+@app.route('/admin/options/dropboxsetup')
+@login_required
+def admin_options_dropbox_setup():
+    flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app.options.app_key, app.options.app_secret)
+    return render_template("admin/dropboxsetup", options=app.options, title="Dropbox setup", url=flow.start())
+
+@app.route('/admin/options/dropboxfinish')
+@login_required
+def admin_options_dropbox_setup():
+    flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app.options.app_key, app.options.app_secret)
+    return render_template("admin/dropboxsetup", options=app.options, title="Dropbox setup", url=flow.start())
