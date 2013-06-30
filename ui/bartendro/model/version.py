a26be4f23bd34864fbffb997dbb666bade6b019e
Robert Kaye <rob@musicbrainz.org>
2013-06-30 17:25:15 +0200

    Added version model

---
+++ ui/bartendro/model/version.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from bartendro import db
+from sqlalchemy.orm import mapper, relationship
+from sqlalchemy import Table, Column, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+class DatabaseVersion(db.Model):
+    """
+    This table stores the version of the Bartendro database
+    """
+
+    __tablename__ = 'version'
+    schema = Column(Integer)
+
+    query = db.session.query_property()
+    def __init__(self, schema = 1)
+        self.schema = schema
+
+    def update(self, schema):
+        self.schema = schema
+
+    def __repr__(self):
+        return "<Version(schema %d)>" % (self.schema)
