25b32a397e0a58311ba1d4ab394b1b34a9e90e62
Robert Kaye <rob@musicbrainz.org>
2013-08-08 19:42:20 +0200

    Remove the brand field from the booze ui. Its not needed now. Fixes #51

--- ui/bartendro/form/booze.py
+++ ui/bartendro/form/booze.py
@@ -6,7 +6,7 @@ from bartendro.model import booze
 class BoozeForm(Form):
     id = HiddenField(u"id", default=0)
     name = TextField(u"Name", [validators.Length(min=3, max=255)])
-    brand = TextField(u"Brand", [validators.Length(min=3, max=255)])
+    brand = TextField(u"Brand") # Currently unused
     desc = TextAreaField(u"Description", [validators.Length(min=3, max=1024)])
     abv = DecimalField(u"ABV", [validators.NumberRange(0, 97)], default=0, places=0)
     type = SelectField(u"Type", [validators.NumberRange(0, len(booze.booze_types))], 
