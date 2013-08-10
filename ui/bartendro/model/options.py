3b24a093583cc9d347071b3d7fa0089a310d1d32
Robert Kaye <rob@musicbrainz.org>
2013-08-10 12:22:16 +0200

    Added options model

---
+++ ui/bartendro/model/options.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from bartendro import db
+from sqlalchemy.orm import mapper, relationship
+from sqlalchemy import Table, Column, Integer, UnicodeText, Text, Index
+
+class Options(db.Model):
+    """
+    Configuration options for Bartendro
+    """
+
+    __tablename__ = 'options'
+    id = Column(Integer, primary_key=True)
+    key = Column(UnicodeText, nullable=False)
+    value = Column(UnicodeText)
+
+    query = db.session.query_property()
+    def __init__(self, key='', value='')
+        self.key = key
+        self.value = value
+
+    def __repr__(self):
+        return "<Option('%s','%s'='%s')>" % (self.id, self.key, self.value)
+
+Index('options_key_ndx', Options.key)
