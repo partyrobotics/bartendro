9f07be188b5ccde911b48cfcf87e1dd3c277fe3a
Robert Kaye <rob@musicbrainz.org>
2013-08-14 19:32:26 +0200

    Make sure the button states in the dispenser always reflect reality. Fixes #48

--- ui/bartendro/view/ws/dispenser.py
+++ ui/bartendro/view/ws/dispenser.py
@@ -3,6 +3,7 @@ import logging
 from time import sleep
 from werkzeug.exceptions import ServiceUnavailable
 from bartendro import app, db, mixer
+from bartendro.global_lock import STATE_ERROR
 from flask import Flask, request
 from flask.ext.login import current_user
 from bartendro.model.drink import Drink
@@ -16,6 +17,9 @@ def ws_dispenser_on(disp):
     if app.options.must_login_to_dispense and not current_user.is_authenticated():
         return "login required"
 
+    if app.mixer.get_state() == STATE_ERROR:
+        return "error state"
+
     if not app.driver.start(disp - 1):
         err = "Failed to start dispenser %d" % disp
         log.error(err)
@@ -28,6 +32,9 @@ def ws_dispenser_off(disp):
     if app.options.must_login_to_dispense and not current_user.is_authenticated():
         return "login required"
 
+    if app.mixer.get_state() == STATE_ERROR:
+        return "error state"
+
     if not app.driver.stop(disp - 1):
         err = "Failed to stop dispenser %d" % disp
         log.error(err)
@@ -37,6 +44,12 @@ def ws_dispenser_off(disp):
 
 @app.route('/ws/dispenser/<int:disp>/test')
 def ws_dispenser_test(disp):
+    if app.options.must_login_to_dispense and not current_user.is_authenticated():
+        return "login required"
+
+    if app.mixer.get_state() == STATE_ERROR:
+        return "error state"
+
     try:
         err = app.mixer.test_dispense(disp - 1)
         if err:
