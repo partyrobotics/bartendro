f7e9aeaa407965c23234f277dc55e4dab004d3fe
Robert Kaye <rob@musicbrainz.org>
2013-08-14 22:50:11 +0200

    Added password recovery feature. Fixes #79

--- ui/bartendro/view/admin/options.py
+++ ui/bartendro/view/admin/options.py
@@ -3,6 +3,7 @@ import time
 import os
 from bartendro import app
 from flask import Flask, request, render_template, Response
+from werkzeug.exceptions import Unauthorized
 from flask.ext.login import login_required
 from bartendro.model.version import DatabaseVersion
 
@@ -10,7 +11,18 @@ from bartendro.model.version import DatabaseVersion
 @login_required
 def admin_options():
     ver = DatabaseVersion.query.one()
+    recover = not request.remote_addr.startswith("10.0.0")
+
     return render_template("admin/options", 
                            options=app.options,
+                           show_passwd_recovery=recover,
                            title="Options", 
                            schema = ver.schema)
+
+@app.route('/admin/lost-passwd')
+def admin_lost_passwd():
+    if request.remote_addr.startswith("10.0.0"):
+        raise Unauthorized
+
+    return render_template("admin/lost-passwd", 
+                           options=app.options)
