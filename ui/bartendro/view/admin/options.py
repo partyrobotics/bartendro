f85ab2d3a67b595d44757188073446562742800d
Robert Kaye <rob@musicbrainz.org>
2014-01-11 16:17:28 +0100

    Added a networ info box that shows the assigned IP addresses

--- ui/bartendro/view/admin/options.py
+++ ui/bartendro/view/admin/options.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import socket
+import fcntl
+import struct
 import time
 import os
 from bartendro import app
@@ -7,16 +10,26 @@ from werkzeug.exceptions import Unauthorized
 from flask.ext.login import login_required
 from bartendro.model.version import DatabaseVersion
 
+def get_ip_address_from_interface(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915,  
+                            struct.pack('256s', ifname[:15]))[20:24])
+
 @app.route('/admin/options')
 @login_required
 def admin_options():
     ver = DatabaseVersion.query.one()
     recover = not request.remote_addr.startswith("10.0.0")
 
+    wlan0 = get_ip_address_from_interface("wlan0")
+    eth0 = get_ip_address_from_interface("eth0")
+
     return render_template("admin/options", 
                            options=app.options,
                            show_passwd_recovery=recover,
                            title="Options", 
+                           eth0=eth0,
+                           wlan0=wlan0,
                            schema = ver.schema)
 
 @app.route('/admin/lost-passwd')
