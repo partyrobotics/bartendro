338003a5b05711c6ed3f340dc3053bfadb3e0171
Robert Kaye <rob@musicbrainz.org>
2014-02-22 18:42:49 +0100

    Fixed crash when no wired connection is present

--- ui/bartendro/view/admin/options.py
+++ ui/bartendro/view/admin/options.py
@@ -12,8 +12,11 @@ from bartendro.model.version import DatabaseVersion
 
 def get_ip_address_from_interface(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915,  
-                            struct.pack('256s', ifname[:15]))[20:24])
+    try:
+        return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915,  
+                                struct.pack('256s', ifname[:15]))[20:24])
+    except IOError:
+        return "[none]"
 
 @app.route('/admin/options')
 @login_required
