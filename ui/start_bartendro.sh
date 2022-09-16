181b1b01fdd85266e15a33368cb49996fb4004b7
Rich Gibson <rich.gibson@gmail.com>
2022-09-15 21:20:15 -0700

    messing with start script

--- ui/start_bartendro.sh
+++ ui/start_bartendro.sh
@@ -4,4 +4,4 @@
 export BARTENDRO_SOFTWARE_ONLY=1
 #python ./bartendro_server.py --debug
 #python ./bartendro_server.py --debug -t ukr.local
-python ./bartendro_server.py --debug -t 192.168.1.168
+python ./bartendro_server.py --debug -t 192.168.1.196
